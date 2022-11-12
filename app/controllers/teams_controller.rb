cd69c147a3f25364f18a0f3c69a862052ef95ae2
sayaka kamei <sayukou316@gmail.com>
2022-11-12 15:46:33 +0900

    権限移動の機能を実装した

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,6 @@
 class TeamsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_team, only: %i[show edit update destroy]
+  before_action :set_team, only: %i[show edit update destroy assign_owner]
 
   def index
     @teams = Team.all
@@ -39,6 +39,12 @@ class TeamsController < ApplicationController
       render :edit
     end
   end
+
+  def assign_owner
+    @team.update(owner_id: params[:owner_id])
+    @user = User.find(@team.owner_id)
+    redirect_to team_path, notice: 'オーナー権限が移動しました!'
+  end  
 
   def destroy
     if current_user.keep_team_id? || current_user.id?
